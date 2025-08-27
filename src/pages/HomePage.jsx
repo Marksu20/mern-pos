6e70fa79e0d13ce005835953bf1fbc2bc2262760
Marksu20 <markjoshuadlcrz@gmail.com>
2025-08-28 00:17:07 +0800

    small changes

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <>
+      
+        {/* <div className="col-span-4 row-span-4 row-start-2">2</div>
+        <div className="row-span-4 col-start-5 row-start-2">3</div> */}
+    </>
+  )
+}
+
+export default HomePage
